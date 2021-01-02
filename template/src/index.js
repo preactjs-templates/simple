38be41046fa147f3c82a3859a436fd52268884d4
Paul <guilbert.paul@gmail.com>
2021-01-02 11:50:55 +0100

    Migrate eslint configuration from eslint-config-synacor to eslint-config-preact

--- template/src/index.js
+++ template/src/index.js
@@ -1,12 +1,9 @@
 import './style';
-import { Component } from 'preact';
 
-export default class App extends Component {
-	render() {
-		return (
-			<div>
-				<h1>Hello, World!</h1>
-			</div>
-		);
-	}
+export default function App() {
+	return (
+		<div>
+			<h1>Hello, World!</h1>
+		</div>
+	);
 }
